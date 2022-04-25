02ca76b1475950a4b5763fc7a6d42f6c36703ead
nick evans <nicholas.evans@gmail.com>
2022-04-25 14:51:34 -0400

    🐛 Fix $MANPATH

--- shell/init.d/01-xdg.sh
+++ shell/init.d/01-xdg.sh
@@ -3,6 +3,10 @@ export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 
+if [ -z "$MANPATH" ]; then
+  MANPATH=$(manpath -g)
+  export MANPATH
+fi
 path_prepend MANPATH         "$XDG_DATA_HOME/man"
 path_prepend PATH            "$HOME/.local/bin"
 path_prepend XDG_DATA_DIRS   "$XDG_DATA_HOME"
