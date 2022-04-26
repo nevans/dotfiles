6577217d314101e917092ec7f526b49014d8bcc3
nicholas a. evans <nicholas.evans@gmail.com>
2022-04-25 22:24:23 -0400

    🔧 clean up XDG env vars, mkdir if missing
    XDG_DATA_HOME doesn't need to be added to XDG_DATA_DIRS.

--- shell/init.d/01-xdg.sh
+++ shell/init.d/01-xdg.sh
@@ -1,13 +1,21 @@
-export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
-export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
-export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
-export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
+export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
+export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 
-path_prepend PATH            "$HOME/.local/bin"
-path_prepend XDG_DATA_DIRS   "$XDG_DATA_HOME"
-path_prepend XDG_CONFIG_DIRS "$XDG_CONFIG_HOME"
-export XDG_DATA_DIR
-export XDG_CONFIG_DIRS
+export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS:-/etc/xdg}"
+export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}"
+
+path_prepend PATH "$HOME/.local/bin"
+
+(
+  umask -S u=rwx,g=,o=
+  mkdir -p "$HOME/.local/bin"
+  mkdir -p "$XDG_CACHE_HOME"
+  mkdir -p "$XDG_CONFIG_HOME"
+  mkdir -p "$XDG_DATA_HOME"
+  mkdir -p "$XDG_STATE_HOME"
+) > /dev/null 2>&1
 
 if [ -z "$MANPATH" ]; then
   export MANPATH="$XDG_DATA_HOME/man:"
