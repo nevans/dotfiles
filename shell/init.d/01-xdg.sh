2632d9e85e1249b2243cb4805440b9b09c427813
nicholas a. evans <nicholas.evans@gmail.com>
2022-04-25 20:38:07 -0400

    🐛 fix $MANPATH when manpath isn't installed

--- shell/init.d/01-xdg.sh
+++ shell/init.d/01-xdg.sh
@@ -3,13 +3,14 @@ export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
 export XDG_STATE_HOME=${XDG_STATE_HOME:-$HOME/.local/state}
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
 
-if [ -z "$MANPATH" ]; then
-  MANPATH=$(manpath -g)
-  export MANPATH
-fi
-path_prepend MANPATH         "$XDG_DATA_HOME/man"
 path_prepend PATH            "$HOME/.local/bin"
 path_prepend XDG_DATA_DIRS   "$XDG_DATA_HOME"
 path_prepend XDG_CONFIG_DIRS "$XDG_CONFIG_HOME"
 export XDG_DATA_DIR
 export XDG_CONFIG_DIRS
+
+if [ -z "$MANPATH" ]; then
+  export MANPATH="$XDG_DATA_HOME/man:"
+else
+  path_prepend MANPATH "$XDG_DATA_HOME/man"
+fi
