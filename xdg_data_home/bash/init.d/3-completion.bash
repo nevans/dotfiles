bf051d99d144ff63d35d73e041b63f7471bca8d8
nicholas a. evans <nevans@410labs.com>
2025-03-13 11:08:58 -0400

    🔧 Add nvm to bashrc

--- xdg_data_home/bash/init.d/3-completion.bash
+++ xdg_data_home/bash/init.d/3-completion.bash
@@ -29,3 +29,9 @@ fi
 if command -v kubectl &>/dev/null; then
   eval "$(kubectl completion bash)"
 fi
+
+############################################################################
+# nvm
+############################################################################
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
